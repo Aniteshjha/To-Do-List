9cee711a31a162f837ad365089e3f2190b65244d
ANITESH <aniteshjha1456@gmail.com>
2022-06-14 16:08:26 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis id erat sed aliquet. Integer eu venenatis est, at finibus nulla. Morbi et libero quis nibh pharetra vestibulum in non metus. Donec vitae velit lectus. Suspendisse tempor, lectus sed dignissim lacinia, nibh lectus hendrerit diam, sed consequat magna velit eget enim. Fusce viverra eleifend nisl, a accumsan eros accumsan a. Nulla vitae bibendum nisl, a luctus purus. Nulla faucibus ex eu metus hendrerit commodo. Aenean dolor quam, aliquam et egestas interdum, consequat pharetra arcu. Quisque mattis sodales felis sed tempus. Sed lacus erat, fermentum vitae nibh in, consequat sollicitudin mi. In vestibulum consequat sem, bibendum auctor ligula commodo varius. Maecenas euismod, odio a commodo dictum, nibh lectus maximus justo, ac sagittis nisl ante ac diam. Integer sed urna dui.</p>
+
+        </div>
+   
+
+    
+    )
+ }
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Footer = () => {
+   
+
+    
+    return (
+        
+            <footer className="bg-dark text-lite py-3" >
+                <p className="text-center">
+                    
+                Copywright &copy; MyTodosList.com     
+                </p>
+                
+
+            </footer>
+            
+        
+    )
+}
